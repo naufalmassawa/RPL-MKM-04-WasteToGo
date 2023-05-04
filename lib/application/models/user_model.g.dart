d7ca1bd064ad002c68a3fc3cc440662fe80ef6da
naufalmassawa <naufal.m@mhs.unsyiah.ac.id>
2023-05-04 17:35:52 +0700

    Adding user model

---
+++ lib/application/models/user_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserModel _$$_UserModelFromJson(Map<String, dynamic> json) => _$_UserModel(
+      username: json['username'] as String?,
+      email: json['email'] as String?,
+      password: json['password'] as String?,
+      role: json['role'] as String?,
+    );
+
+Map<String, dynamic> _$$_UserModelToJson(_$_UserModel instance) =>
+    <String, dynamic>{
+      'username': instance.username,
+      'email': instance.email,
+      'password': instance.password,
+      'role': instance.role,
+    };
