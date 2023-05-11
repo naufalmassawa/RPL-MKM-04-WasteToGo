c0a98cf071d3930630cdcfe31449595cbde56e01
Arip <arif1601@mhs.unsyiah.ac.id>
2023-05-11 22:23:30 +0700

    adding main and injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:waste_to_go/presentation/utils/constants.dart';
 
 import 'firebase_options.dart';
-import 'injection.dart' as injection;
 import 'presentation/blocs/auth_bloc.dart';
 import 'presentation/blocs/location_bloc.dart';
-import 'presentation/blocs/page_cubit.dart';
+import 'presentation/blocs/page_and_kategory_cubit.dart';
 import 'presentation/blocs/product_bloc.dart';
 import 'presentation/routes/app_routes.dart';
+import 'injection.dart' as injection;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,17 +34,28 @@ class MyApp extends StatelessWidget {
           create: (_) => injection.locator<PageCubit>(),
         ),
         BlocProvider(
+          create: (_) => injection.locator<CategoryCubit>(),
+        ),
+        BlocProvider(
           create: (_) => injection.locator<LocationBloc>(),
         ),
         BlocProvider(
           create: (_) => injection.locator<ProductBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => injection.locator<GetFreeProductBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => injection.locator<GetExpiredProductBloc>(),
         ),
       ],
       child: MaterialApp.router(
         title: 'Waste To Go',
         debugShowCheckedModeBanner: false,
         // Theme
-        theme: ThemeData.light(),
+        theme: ThemeData.light().copyWith(
+          colorScheme: kColorScheme.copyWith(),
+        ),
         // Routing
         routerDelegate: router.routerDelegate,
         routeInformationParser: router.routeInformationParser,
