499f0d4a2760e4ca3fecbd6c580e3983d7f28002
Arip <arif1601@mhs.unsyiah.ac.id>
2023-05-11 21:32:04 +0700

    adding gmaps on ios apps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    GMSServices.provideAPIKey("AIzaSyBDASs-kQrbThSg-dfrW6ymoYbu-TqoTc4")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
